30786e44d3430a734934dc4a6f069e80237154b7
darothen <darothen@mit.edu>
2017-04-25 08:51:50 -0400

    Hard-coding RTD theme to resolve some weird config issue

--- doc/conf.py
+++ doc/conf.py
@@ -100,11 +100,12 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    # The theme to use for HTML and HTML Help pages.  See the documentation for
-    # a list of builtin themes.
-    #
-    html_theme = 'alabaster'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'default'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
